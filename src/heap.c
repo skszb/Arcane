8e305e51e8733f3125cd21b39db77edfb7e16857
ZB <13489103779@163.com>
2022-12-04 17:37:15 -0500

    fixed heap memory leak reporter

--- src/heap.c
+++ src/heap.c
@@ -11,6 +11,8 @@
 #include <windows.h>
 #include <DbgHelp.h>
 
+typedef struct alloc_info_t alloc_info_t;
+
 typedef struct arena_t
 {
 	pool_t pool;
@@ -45,15 +47,32 @@ heap_t* heap_create(size_t grow_increment)
 	return heap;
 }
 
+
+
+#define BACKTRACE_MAX_NUM 8
+#define FRAME_SKIP 1	// skip to where the leak happens
+
+typedef struct alloc_info_t
+{
+	HANDLE process;
+	USHORT stack_num;
+	PVOID stack_backtrace[BACKTRACE_MAX_NUM];
+} alloc_info_t;
+
 void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 {
+
 	mutex_lock(heap->mutex);
 
-	void* address = tlsf_memalign(heap->tlsf, alignment, size);
+	// The the backtrace information of the caller
+	size_t total_size = size + sizeof(alloc_info_t);
+
+	void* address = tlsf_memalign(heap->tlsf, alignment, total_size);
+
 	if (!address)
 	{
 		size_t arena_size =
-			__max(heap->grow_increment, size * 2) +
+			__max(heap->grow_increment, size * 2) + 
 			sizeof(arena_t);
 		arena_t* arena = VirtualAlloc(NULL,
 			arena_size + tlsf_pool_overhead(),
@@ -71,24 +90,27 @@ void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 		arena->next = heap->arena;
 		heap->arena = arena;
 
-		address = tlsf_memalign(heap->tlsf, alignment, size);
+		address = tlsf_memalign(heap->tlsf, alignment, total_size);
 	}
 
 	mutex_unlock(heap->mutex);
 
+	alloc_info_t* info = address;
+	info->process = GetCurrentProcess();
+	info->stack_num = CaptureStackBackTrace(FRAME_SKIP, BACKTRACE_MAX_NUM, info->stack_backtrace, NULL);
+
+	address = info + 1;
 	return address;
 }
 
 void heap_free(heap_t* heap, void* address)
 {
 	mutex_lock(heap->mutex);
-	tlsf_free(heap->tlsf, address);
+	tlsf_free(heap->tlsf, (alloc_info_t*)address-1);
 	mutex_unlock(heap->mutex);
 }
 
 // Special walker function used for reporting leak size and callstack
-#define BACK_TRACE_MAX_NUM 16
-#define FRAME_SKIP 4	// skip to where the leak happens
 #define CALLER_NAME_MAX 32	// the size of the buffer that holds the caller's name
 void report_leak_walker(void* ptr, size_t size, int used, void* user)
 {
@@ -97,26 +119,25 @@ void report_leak_walker(void* ptr, size_t size, int used, void* user)
 		static const double byte_size = 1024;
 		double bytes = size / byte_size;
 		printf("Memory leak of size %.2f bytes with callstack:\n", bytes);
-		PVOID back_trace[BACK_TRACE_MAX_NUM];
-		USHORT stack_num = CaptureStackBackTrace(FRAME_SKIP, BACK_TRACE_MAX_NUM, back_trace, NULL);
-
-		HANDLE process = GetCurrentProcess();
-		SymInitialize(process, NULL, TRUE);
+		
+		alloc_info_t* info = ptr;
+		
+		SymInitialize(info->process, NULL, TRUE);
 		IMAGEHLP_SYMBOL* symbol = (IMAGEHLP_SYMBOL*)malloc(sizeof(IMAGEHLP_SYMBOL64) + (CALLER_NAME_MAX) * sizeof(CHAR));
 		symbol->SizeOfStruct = sizeof(IMAGEHLP_SYMBOL64);
 		symbol->Size = 0;
 		symbol->MaxNameLength = CALLER_NAME_MAX;
 
-		for (USHORT frame_num = 0; frame_num < stack_num; frame_num++)
+		for (USHORT frame_num = 0; frame_num < info->stack_num; frame_num++)
 		{
-			DWORD64 address = (DWORD64)back_trace[frame_num];
-			SymGetSymFromAddr64(process, address, NULL, symbol);
+			DWORD64 address = (DWORD64)(info->stack_backtrace)[frame_num];
+			SymGetSymFromAddr64(info->process, address, NULL, symbol);
 			printf("[%d] = %s\n", frame_num, symbol->Name);
 
 			if (strcmp(symbol->Name, "main") == 0) break;
 		}
 		free(symbol);
-		SymCleanup(process);
+		SymCleanup(info->process);
 	}
 }
 
